e9ce59ce65e38770c18159803326d0d6ab1ba0ef
IHA089 <67224300+Rohitash465@users.noreply.github.com>
2025-02-21 20:28:45 +0530

    Update MailServerIHA089.py

--- MailServerIHA089.py
+++ MailServerIHA089.py
@@ -10,10 +10,11 @@ log.setLevel(logging.ERROR)
 
 MailServerIHA089 = Flask(__name__)
 
-mail_loc = "/IHA089-Mail/"
+mail_loc = "IHA089-Mail"
 
 def create_database():
-    conn = sqlite3.connect('mail_users.db')
+    db_path = os.path.join(os.getcwd(), mail_loc, "mail_users.db")
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -41,27 +42,27 @@ def create_database():
     )
     ''')
 
-    #cursor.execute("INSERT INTO Email_data(email, sender, subject, bodycontent) VALUES ('test@iha089.org.in', 'IHA089', 'verify your account', '<h2>Verify Your Account</h2><p>Click the button below to verify your email address:</p><a href=\"https://example.com/verify?token=YOUR_TOKEN\">Verify Your Account</a><p>If you did not request this, please ignore this email.</p>')")
     conn.commit()
     conn.close()
 
 def check_database():
-    db_path = os.getcwd()+mail_loc+'mail_users.db'
+    db_path = os.path.join(os.getcwd(), mail_loc, "mail_users.db")
+   
     if not os.path.isfile(db_path):
+        print("not present at "+db_path)
         create_database()
-
 
 check_database()
 
 def get_db_connection():
-    db_path = os.getcwd()+mail_loc+'mail_users.db'
+    db_path = os.path.join(os.getcwd(), mail_loc, "mail_users.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def get_email_data(email):
-    db_path = os.getcwd()+mail_loc+'mail_users.db'
+    db_path = os.path.join(os.getcwd(), mail_loc, "mail_users.db")
     conn = sqlite3.connect(db_path)  
     cursor = conn.cursor()
     query = "SELECT id, email, sender, subject, bodycontent, timestamp FROM Email_data WHERE email = '"+email+"'"
@@ -161,10 +162,7 @@ def dcb8df93f8885473ad69681e82c423163edca1b13cf2f4c39c1956b4d32b4275():
 
         if not email or not sender or not subject or not bodycontent:
             return jsonify({"error": "Invalid request"}), 400
-        # print(f"email: {email}")
-        # print(f"sender: {sender}")
-        # print(f"subject: {subject}")
-        # print(f"bodycontent:{bodycontent}")
+            
         query = f"INSERT INTO Email_data (email, sender, subject, bodycontent) VALUES ('{email}', '{sender}', '{subject}', '{bodycontent}')".format(email, sender, subject, bodycontent)
         conn = get_db_connection()
         cursor = conn.cursor()
